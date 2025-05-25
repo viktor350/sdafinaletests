b7882a5c2950b9a2dbe3924e506931aa4bd6139d
Viktor <veliky.viktor@gmail.com>
2025-05-25 13:39:51 +0200

    add new class SdaTest

--- src/main/java/demotest/SdaTest.java
+++ src/main/java/demotest/SdaTest.java
@@ -1,4 +1,37 @@
 package demotest;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SdaTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\velik\\Desktop\\Nový priečinok (2)\\SdaFinalTests\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void sdaTest(){
+        //toto mi najde stranku Tutorial point
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+
+        //toto mi najde na stranke prvek a vypiše meno
+        driver.findElement(By.id("name")).sendKeys("Viktor");
+
+    }
+
+
+
+
+
+
+
+
 }
