a9af509260baf5eada67a522b7ffd6744ab3f888
Viktor <veliky.viktor@gmail.com>
2025-05-25 13:59:23 +0200

    add new class SdaTest

--- src/main/java/demotest/SdaTest.java
+++ src/main/java/demotest/SdaTest.java
@@ -3,6 +3,7 @@ package demotest;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,8 +23,20 @@ public class SdaTest {
         //toto mi najde stranku Tutorial point
         driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
 
+        driver.manage().window().setSize(new Dimension(1936, 1048));
         //toto mi najde na stranke prvek a vypiše meno
         driver.findElement(By.id("name")).sendKeys("Viktor");
+
+        driver.findElement(By.id("email")).sendKeys("veliky.viktor@gmail.com");
+
+
+
+
+
+
+
+
+
 
     }
 
